6bf6f880e7601e9383894f7eff4933fc0efe2140
bgabot89 <bgabot89@gmail.com>
2016-04-12 17:07:59 -0700

    added more functionality to quiz

--- public/js/test.js
+++ public/js/test.js
@@ -2,16 +2,26 @@ $( document ).ready(function() {
 
 // Quiz result options in a separate object for flexibility
 var resultOptions = [
-    {   title: 'You Are a Knight',
-        desc: '<p>Here, have an Archer</p><img src="http://i.imgur.com/tXTjD9k.jpg"/>'},
-    {   title: 'You Are an Archer',
-        desc: '<p>Here, have an Archer</p><img src="http://i.imgur.com/dipkE0v.jpg"/>'},
-    {   title: 'You Are a Mage',
-        desc: '<p>Here, have an Archer</p><img src="http://i.imgur.com/WXox0Yv.jpg"/>'},
-    {   title: 'You Are an Priest',
-        desc: '<p>Here, have an Archer</p><img src="http://i.imgur.com/NH5cunw.png"/>'},
-    {   title: 'You Are A Thief',
-        desc: '<p>Here, have an Archer</p><img src="http://i.imgur.com/NH5cunw.png"/>'}
+    {   title: 'Knight',
+        image: '<img class = "result-pic" id = "img" src="/static/css/images/barb.png"/>',
+        // image: '/static/css/images/barb.png',
+        desc: '<p class="info">You are a knight, a skilled warrior specializing in heavy armor and weaponry. As a knight, you are portrayed as a brave warrior who will defend your town and kingdom from any threats of danger. </p>'},
+    {   title: 'Ranger',
+        image: '<img class = "result-pic" id = "img" src="/static/css/images/ranger.png"/>',
+        // image: '/static/css/images/ranger.png',
+        desc: '<p class="info">You are a ranger, a warrior skilled with the bow. As a ranger, you have excellent skills in used in the  wilderness including: hunting or training wild animals. </p>'},
+    {   title: 'Wizard',
+        image: '<img class = "result-pic" id = "img" src="/static/css/images/wiz.png"/>',
+        // image: '/static/css/images/wiz.png',
+        desc: '<p class="info">You are a wizard, a powerful spellcaster specializing in arcane magic. You specialize in using magic wands and tomes to cast your magic. Although wizards are powerful with magic, they have poor physical combat skills</p>'},
+    {   title: 'Cleric',
+        image: '<img class = "result-pic" id = "img" src="/static/css/images/cleric.png"/>',
+        // image: '/static/css/images/cleric.png',
+        desc: '<p class="info">You are a Cleric, a skilled class specializing in divine magic. Clerics are powerful healers due to the large number of healing and curative magics available to them.</p>'},
+    {   title: 'Rogue',
+        image: '<img class = "result-pic" id = "img" src="/static/css/images/rogue.png"/>',
+        // image: '/static/css/images/rogue.png',
+        desc: '<p class="info">You are a Rogue, a versatile character specializing in sneaky combat and tactics. Rogues are stealthy and dextrous and are capable of disarming many traps and locks. Their main weapon of choice is the dagger.</p>'}
 ];
 
 //Global variable to use for number of results, change if needed
@@ -94,7 +104,14 @@ function calcResults(totalScore) {
     if (quizSteps.find('.active').length == quizSteps.length){
       $('#results').show();
         var resultsTitle = $('#results h1'),
-            resultsDesc = $('#results .desc');
+            resultsDesc = $('#results .desc'),
+            resultsImag1 = $('#results #pic1'),
+            resultsTitle2 = $('#results h2'),
+            resultsDesc2 = $('#results .desc2'),
+            resultsImag2 = $('#results #pic2'),
+            resultsTitle3 = $('#results h3'),
+            resultsDesc3 = $('#results .desc3'),
+            resultsImag3 = $('#results #pic3');
 
         // calc lowest possible score
         var lowestScoreArray = $('#quizzie .low-value').map(function() {
@@ -122,10 +139,41 @@ function calcResults(totalScore) {
         while (n < numResults) {
             increment = minScore + (interval * n);
             if (totalScore <= increment) {
+                //hide current slide to show results
+                $('.step'+[i]).hide();
                 // populate results onto page
                 resultsTitle.replaceWith("<h1>" + resultOptions[n].title + "</h1>");
-                console.log(totalScore);
+                console.log('number in array is' + ' ' + n);
                 resultsDesc.replaceWith("<p class='desc'>" + resultOptions[n].desc + "</p>");
+                resultsImag1.replaceWith("<span id='pic1'>" + resultOptions[n].image + "</span>");
+                // if end of array, then reverse
+                  if (n == 4) {
+                    resultsTitle2.replaceWith("<h2>" + resultOptions[n-1].title + "</h2>");
+                    resultsDesc2.replaceWith("<p class='desc2'>" + resultOptions[n-1].desc + "</p>");
+                    resultsImag2.replaceWith("<span id='pic2'>" + resultOptions[n-1].image + "</span>");
+                    console.log($(this).width());
+                    resultsTitle3.replaceWith("<h3>" + resultOptions[n-2].title + "</h3>");
+                    resultsDesc3.replaceWith("<p class='desc3'>" + resultOptions[n-2].desc + "</p>");
+                    resultsImag3.replaceWith("<span id='pic3'>" + resultOptions[n-2].image + "</span>");
+                }
+                else if (n == 3) {
+                    resultsTitle2.replaceWith("<h2>" + resultOptions[n+1].title + "</h2>");
+                    resultsDesc2.replaceWith("<p class='desc2'>" + resultOptions[n+1].desc + "</p>");
+                    var results = resultsImag2.replaceWith("<span id='pic2'>" + resultOptions[n+1].image + "</span>");
+                    console.log(resultOptions[n+1].image.style);
+                    resultsTitle3.replaceWith("<h3>" + resultOptions[n-1].title + "</h3>");
+                    resultsDesc3.replaceWith("<p class='desc3'>" + resultOptions[n-1].desc + "</p>");
+                    resultsImag3.replaceWith("<span id='pic3'>" + resultOptions[n-1].image + "</span>");
+                  }
+              else {
+                resultsTitle2.replaceWith("<h2>" + resultOptions[n+1].title + "</h2>");
+                resultsDesc2.replaceWith("<p class='desc2'>" + resultOptions[n+1].desc + "</p>");
+                resultsImag2.replaceWith("<span id='pic2'>" + resultOptions[n+1].image + "</span>");
+                console.log($(this).width());
+                resultsTitle3.replaceWith("<h3>" + resultOptions[n+2].title + "</h3>");
+                resultsDesc3.replaceWith("<p class='desc3'>" + resultOptions[n+2].desc + "</p>");
+                resultsImag3.replaceWith("<span id='pic3'>" + resultOptions[n+2].image + "</span>");
+              }
                 return;
             } else {
                 n++;
@@ -133,4 +181,12 @@ function calcResults(totalScore) {
         }
     }
 }
+
+//event handlers when clicking on image
+$('#pic1').click(function() {
+  alert('result-pic');
+  // document.getElementById('play').style.visibility = "hidden";
+});
+
+
 });
